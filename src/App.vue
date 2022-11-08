8283bf558ed18e8a377a5a01884ed44b1b514597
KRYLOV <alexkrylov.2003@yandex.ru>
2022-11-08 19:25:53 +0300

    Сделан 'аккордеон' с пользователями

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <usersList/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import usersList from './components/usersList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    usersList,
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.container {
+  padding: 20px;
+  border-radius: 30px;
+  width: 620px;
+  height: 90vh;
 }
 </style>
---
+++ src/components/usersList.vue
@@ -0,0 +1,116 @@
+<template>
+     <div class="usersList">
+
+
+          <div class="user " v-for="user in users" :key="user.id">
+               <div class="userMain">
+                    <p> {{ user.name }} </p>
+                    <button @click="isOpen === user.id ? isOpen = false : isOpen = user.id">+</button>
+               </div>
+
+               <div class="userInfo" v-if='isOpen === user.id'>
+                    <p>Имя: {{user.name}}</p>
+                    <p>Никнейм: {{user.username}}</p>
+                    <p>Email: {{user.email}}</p>
+               </div>
+          </div>
+
+     </div>
+</template>
+
+
+<script>
+     export default {
+          data() {
+               return {
+                    name: 'Имя Фамилин',
+                    isOpen: false,
+                    users: [],
+               }
+          },
+          mounted() {
+               this.userInfo();
+          },
+          methods: {
+               async userInfo() {
+                    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                    const result = await response.json();
+
+                    result.forEach(item=> { 
+                         
+                         this.users.push({
+                              id: item.id, 
+                              name: item.name,
+                              username: item.username,
+                              email: item.email,
+                         })
+
+                    });
+
+                
+               }
+          },
+
+     }
+</script>
+
+
+<style>
+     .usersList {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background-color: #8db5cc;
+          border-radius: 30px;
+          padding: 20px 0;
+          transition: all 0.9s;
+     }
+     .userMain {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          width: 520px;
+          box-shadow: 0 0 11px rgba(122, 122, 122, 0.342);
+          border-radius: 20px;
+          padding: 8px 15px;
+          background-color: white;
+          margin: 11px 0;
+          font-weight: bold;
+          font-size: 23px;
+     }
+     button {
+          border-radius: 30px;
+          background-color: white;
+          box-shadow: 0 0 10px rgba(46, 46, 46, 0.342);
+          border: none;
+          cursor: pointer;
+          font-size: 25px;
+          width: 40px;
+          height: 40px;
+          font-weight: bold;
+          transition: all 0.2s;
+     }
+     button:hover {
+          background-color: rgb(236, 236, 236);
+          transition: all 0.2s;
+     }
+     .userInfo {
+          width: 520px;
+          box-shadow: 0 0 11px rgba(122, 122, 122, 0.342);
+          border-radius: 20px;
+          background-color: white;
+          padding: 15px;
+          transition: all ease 3s;
+     }
+     .userInfo p {
+          display: inline-block;
+          padding: 7px;
+          margin: 5px;
+          border-radius: 10px;
+          box-shadow: 0 0 11px rgba(122, 122, 122, 0.342);
+          color: rgb(88, 88, 88);
+          font-weight: bold;
+     }
+</style>
